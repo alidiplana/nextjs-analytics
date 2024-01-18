f66e900c3a2f5b7a32f7a83b51aef995534a92db
Ali <alidiplana@gmail.com>
2024-01-18 13:13:45 +0500

    GA library

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,11 @@ import styles from "../styles/Home.module.css";
 import Navbar from "../components/navigation";
 import ReactGA from "react-ga";
 const TRACKING_ID = "UA-XXXXX-X";
+ReactGA.initialize(TRACKING_ID);
 
 export default function Home() {
   return (
     <>
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-SJZ6E6TZD7"
-      ></script>
-      <script>
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'G-SJZ6E6TZD7');
-      </script>
       <Navbar />
 
       <div style={{ marginTop: "10px", fontSize: "20px" }}>
