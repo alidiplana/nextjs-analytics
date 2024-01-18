6eb3010396472953a69b6e45c668abb24241ae96
Ali <alidiplana@gmail.com>
2024-01-18 15:15:16 +0500

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Navbar from "../components/navigation";
-import ReactGA from "react-ga";
-const TRACKING_ID = "UA-XXXXX-X";
-ReactGA.initialize(TRACKING_ID);
+import Script from "next/script";
 
 export default function Home() {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+
       <Navbar />
 
       <div style={{ marginTop: "10px", fontSize: "20px" }}>
