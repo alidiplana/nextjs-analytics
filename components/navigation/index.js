644b25dac82e41f926a1ae1391895f1ed4f8a412
Ali <alidiplana@gmail.com>
2024-01-18 15:23:12 +0500

    changes

--- components/navigation/index.js
+++ components/navigation/index.js
@@ -1,10 +1,28 @@
 export default function Navbar() {
   return (
-    <div style={{ display: "flex", gap: "10px" }}>
-      <a href="/home">Home</a>
-      <a href="/aboutus">About Us</a>
-      <a href="/contact">Contact</a>
-      <a href="/product">Product</a>
-    </div>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+
+      <div style={{ display: "flex", gap: "10px" }}>
+        <a href="/home">Home</a>
+        <a href="/aboutus">About Us</a>
+        <a href="/contact">Contact</a>
+        <a href="/product">Product</a>
+      </div>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,6 @@ import Script from "next/script";
 export default function Home() {
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script strategy="lazyOnload">
-        {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-      </Script>
-
       <Navbar />
 
       <div style={{ marginTop: "10px", fontSize: "20px" }}>
